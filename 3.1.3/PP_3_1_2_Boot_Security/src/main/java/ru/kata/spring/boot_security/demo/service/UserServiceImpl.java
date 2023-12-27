e2412ecc2a059973687fbf996da45610c28abc78
creatorapkfiles <67171093+creatorapkfiles@users.noreply.github.com>
2023-12-27 21:54:33 +0300

    Update UserServiceImpl.java

--- 3.1.3/PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ 3.1.3/PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -72,4 +72,13 @@ public class UserServiceImpl implements UserService {
     public List<Role> getRoles() {
         return roleRepository.findAll();
     }
+
+        @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails userDetails = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User doesn't exist"));
+
+        return new org.springframework.security.core.userdetails.User(userDetails.getUsername(),
+                userDetails.getPassword(), userDetails.getAuthorities());
+    }
 }
